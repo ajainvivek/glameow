37059658dd0a05fd873febb5c4b375bd05093058
Ajain Vivek <ajainvivek07@gmail.com>
2018-08-28 23:58:34 +1000

    update cli

--- cli.js
+++ cli.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
-var argv = require('minimist')(process.argv.slice(2))
+var argv = require('minimist')(process.argv.slice(2), {
+    string: ['path', 'destination', 'pwd'],
+    boolean: ['help'],
+    '--': true
+})
 var glameow = require('./src/index')
 
-if (argv.root && argv.destination) {
-    glameow({
-        root: argv.root,
-        destination: argv.destination,
-        pwd: argv.pwd
-    })
+if (argv._[0] !== 'generate') {
+    console.info('Invalid options - please check glameow --help')
+    return;
+} else {
+    if (argv._[1] === 'component' || argv._[1] === 'page') {
+        glameow({
+            path: argv.path,
+            destination: argv.destination,
+            pwd: argv.pwd,
+            type: argv._[1]
+        })
+    } else {
+        console.info('Invalid options - please check glameow --help')
+        return;
+    }
 }
+
